ae97f7b5e2d4ab40ac2708a0933a15e509fb0289
Attie Grande <attie@attie.co.uk>
2018-05-28 13:27:58 +0100

    added a check that the input file actually exists...

--- clidriver.py
+++ clidriver.py
@@ -1,3 +1,5 @@
+import os
+
 from aax_to_ogg.args import config
 from aax_to_ogg.filehandler import FileHandler
 
@@ -6,6 +8,11 @@ def main():
 
     for filename in config.files:
         print('Processing [%s]...' % ( filename ))
+
+        if not os.path.exists(filename):
+            print('   NOTE: %s doesn\'t exist...' % ( filename ))
+            continue
+
         try:
             handler.handle_file(filename)
         except Exception as e:
